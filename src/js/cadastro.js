1d35266ba0b0f9b47402a42a31535789bbf9b6e0
Allan <teixeiraallan312@gmail.com>
2025-05-21 22:07:08 -0300

    atualizando script, html e css

---
+++ src/js/cadastro.js
@@ -0,0 +1,17 @@
+var formProduto =document.getElementById("form-produto")
+
+function cadastrarProduto(event) {
+
+    event.preventDefault()
+
+    var produto = document.getElementById("nome-produto").value
+
+    var quantidade = document.getElementById("quantidade-produto").value
+
+    console.log(produto)
+    console.log(quantidade)
+
+    console.log("Função cadastrarProduto chamada")
+}
+
+formProduto.addEventListener("submit", cadastrarProduto)
